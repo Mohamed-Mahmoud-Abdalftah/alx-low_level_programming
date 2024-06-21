9e0c6c96e1e5bfbeb7a649ca59240bd4ceda776d
Mohamed <mohamed.m.abdalftah@gmail.com>
2024-06-21 11:42:29 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all hexadecimal numbers in lowercase followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char hex_digit;
+
+    for (hex_digit = '0'; hex_digit <= '9'; hex_digit++)
+    {
+        putchar(hex_digit);
+    }
+
+    for (hex_digit = 'a'; hex_digit <= 'f'; hex_digit++)
+    {
+        putchar(hex_digit);
+    }
+
+    putchar('\n');
+
+    return (0);
+}
+
